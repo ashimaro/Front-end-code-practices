9e070cc7f04fb903966cbc23adef9aab9afbda84
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-02-23 20:30:29 +0800

    create delete button for list

--- ToDo list/index.js
+++ ToDo list/index.js
@@ -1,6 +1,32 @@
-function addTodo(){
-    var todoinput = document.getElementById("new=todo");
+function addTodo() {
+    var todoInput = document.getElementById("new-todo");
+    var todoList = document.getElementById("todo-list");
 
-//add new todo
 
-}
+    var todolist = []; //create an empty array to store new to-do item.
+    todolist.push(todoInput.value);
+
+    //create a new list item 
+
+    var newtodo = document.createElement("li");
+    newtodo.innerText = todoInput.value;
+
+    //create delete button for new list item
+    var deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.classList.add("delete-button");
+
+//append delete button to new list item
+newtodo.appendChild(deleteButton);
+
+    //add the new list item to todo list
+    todoList.appendChild(newtodo);
+
+    //clear the input field by set the input to an empty string
+    todoInput.value = "";
+
+    deleteButton.addEventListener("click", function () {
+        newtodo.remove();
+    });
+
+} 
